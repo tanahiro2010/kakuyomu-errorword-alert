c8fa7bd8e4ba4cd40896c9e0a191e1b1e8a50212
tanahiro2010 <herentongkegu087@gmail.com>
2025-07-26 14:01:36 +0900

    [update] いろいろアップデート

--- src/layout.ts
+++
@@ -1,5 +0,0 @@
-const tailwind: HTMLScriptElement = document.createElement('script');
-tailwind.src = "//cdn.tailwindcss.com";
-
-const head: HTMLHeadElement = document.head;
-head.appendChild(tailwind);
